5182a0568428c65bedbb3f0ee350bcf0e3a4fb14
aditya patel <aditya.patel@nonceblox.com>
2022-08-09 23:15:54 +0530

    update: app tsx and module filr with notesGroup and createNote components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
-
+// styles
 import styles from "./App.module.scss";
+// components
+import CreateNote from "./components/Elements/CreateNote/CreateNote";
 import Note from "./components/Elements/Note/Note";
+import NotesGroup from "./components/Elements/NotesGroup/NotesGroup";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className={styles.App}>
-      <Note />
+      <div className={styles.sideBarContainer}></div>
+      <div className={styles.noteTakingContainer}>
+        <CreateNote />
+
+        <NotesGroup />
+      </div>
     </div>
   );
 }
