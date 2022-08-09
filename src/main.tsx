efe515baebc74fe93636ccaf2d8b5999f32b95d6
aditya patel <aditya.patel@nonceblox.com>
2022-08-09 17:58:37 +0530

    update: main.tsx file with redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// redux
+import { store } from "./store";
+import { Provider } from "react-redux";
+// styles
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
-)
+);
