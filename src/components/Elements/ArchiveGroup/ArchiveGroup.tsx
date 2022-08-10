342538f0d780bfcd29b717de0b3426de63743c16
aditya patel <aditya.patel@nonceblox.com>
2022-08-10 10:40:06 +0530

    create: archive group component with required logic

---
+++ src/components/Elements/ArchiveGroup/ArchiveGroup.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useEffect, useState } from "react";
+// libraries
+import { useSelector } from "react-redux";
+// slices
+import { INote, selectedNotes } from "../../../slices/notes";
+// components
+import Note from "../Note/Note";
+// styles
+import styles from "./ArchiveGroup.module.scss";
+
+const ArchiveGroup = () => {
+  const notes = useSelector(selectedNotes);
+
+  const [numOfNotesInArchive, setNumOfNotesInArchive] = useState(0);
+
+  const getNotesInArchive = useCallback(
+    () =>
+      notes.reduce((acc, { inArchive }) => {
+        const value = inArchive ? 1 : 0;
+        return (acc += value);
+      }, 0),
+    [notes]
+  );
+
+  useEffect(() => {
+    setNumOfNotesInArchive(getNotesInArchive());
+  }, [notes]);
+
+  if (numOfNotesInArchive === 0) return <></>;
+  console.log(numOfNotesInArchive);
+
+  return (
+    <div className={styles.outerContainer}>
+      {/* <h2></h2> */}
+      <div className={styles.container}>
+        {notes.map(
+          ({
+            id,
+            title,
+            note,
+            bgColor,
+            images,
+            isPinned,
+            inTrash,
+            inArchive,
+          }: INote) => {
+            if (inArchive)
+              return (
+                <Note
+                  key={id}
+                  id={id}
+                  title={title}
+                  note={note}
+                  bgColor={bgColor}
+                  images={images}
+                  isPinned={isPinned}
+                  inTrash={inTrash}
+                  inArchive={inArchive}
+                />
+              );
+          }
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ArchiveGroup;
