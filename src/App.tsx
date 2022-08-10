e577a977171f68e8f53a5557f814667b0a5ec18e
aditya patel <aditya.patel@nonceblox.com>
2022-08-10 10:38:58 +0530

    update: app tsx file to include trashgroup and archive group compoenet as well as sidebarcomponent and styling also changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
 import { useState } from "react";
 // styles
 import styles from "./App.module.scss";
+import ArchiveGroup from "./components/Elements/ArchiveGroup/ArchiveGroup";
 // components
 import CreateNote from "./components/Elements/CreateNote/CreateNote";
-import Note from "./components/Elements/Note/Note";
 import NotesGroup from "./components/Elements/NotesGroup/NotesGroup";
+import PinGroup from "./components/Elements/PinGroup/PinGroup";
+import Sidebar from "./components/Elements/Sidebar/Sidebar";
+import TrashGroup from "./components/Elements/TrashGroup/TrashGroup";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedSideBarTopic, setSelectedSideBarTopic] = useState<
+    "notes" | "archive" | "trash"
+  >("notes");
+
+  const handleSideBarTopicUpdate = (topic: "notes" | "archive" | "trash") => {
+    setSelectedSideBarTopic(topic);
+  };
 
   return (
     <div className={styles.App}>
-      <div className={styles.sideBarContainer}></div>
-      <div className={styles.noteTakingContainer}>
-        <CreateNote />
+      <Sidebar
+        selectedSideBarTopic={selectedSideBarTopic}
+        handleSideBarTopicUpdate={handleSideBarTopicUpdate}
+      />
 
-        <NotesGroup />
+      <div className={styles.noteTakingContainer}>
+        {selectedSideBarTopic === "notes" && (
+          <>
+            <CreateNote />
+            <PinGroup />
+            <NotesGroup />
+          </>
+        )}
+
+        {selectedSideBarTopic === "trash" && <TrashGroup />}
+
+        {selectedSideBarTopic === "archive" && <ArchiveGroup />}
       </div>
     </div>
   );
